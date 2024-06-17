e2f58eaf7bc48ff3cc336fd9a4db21b8d0c3c63e
micaela <hirschfeldmicaela904@gmail.com>
2024-06-16 21:32:31 -0300

    api

---
+++ api.js
@@ -0,0 +1,27 @@
+let contenedorPersonajes = document.getElementById("personajes")
+
+fetch("https://rickandmortyapi.com/api/character")
+.then((response)=>response.json())
+.then((datos)=>{
+
+    console.log(datos)
+    console.log(datos.results)
+
+    datos.results.forEach((elementos) => {
+        
+
+        const contenedorCreado = document.createElement('div')
+
+        contenedorCreado.innerHTML = `
+            <h4>${elementos.name}</h4>
+            <img src="${elementos.image}">
+            
+        `;
+
+
+       contenedorPersonajes.append(contenedorCreado)
+
+
+    });
+
+})
