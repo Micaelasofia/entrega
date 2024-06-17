8d791fa5849bd09aa7577f73f298bbe162beac1b
micaela <hirschfeldmicaela904@gmail.com>
2024-06-17 20:22:06 -0300

    cambios

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let inputUsuario = document.getElementById("correo")
-let inputPassword = document.getElementById("password")
-let boton= document.getElementById("boton")
-
-
-
-
-boton.addEventListener("click",(e)=>{
-    e.preventDefault()
-    
-if(inputUsuario.value==="" || inputPassword.value===""){
-    Swal.fire({
-        title: '¡Atención!',
-        text: '¡Por favor complete todos los campos!',
-        icon: 'warning',
-        confirmButtonText: 'OK'
-      })
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    if (email === "" || password === "") {
+        Swal.fire({
+            title: 'Error',
+            text: 'Por favor, completa todos los campos.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    } else {
+        Swal.fire({
+            title: 'Éxito',
+            text: 'Iniciaste sesión correctamente.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    }
+  });
